924583ef06ab6bb23b578d2b3229deb4e762c2a4
mxcom <mxprogramming@protonmail.com>
2022-07-06 15:46:20 +0200

    minor change

--- Sheet7/S7E3/src/main/java/com/example/s7e3/ChangeQuantity.java
+++ Sheet7/S7E3/src/main/java/com/example/s7e3/ChangeQuantity.java
@@ -18,22 +18,24 @@ public class ChangeQuantity extends HttpServlet {
         // get selected fund
         HttpSession session = request.getSession(true);
         String search_stock = request.getParameter("stock_name");
-        String new_quantity = request.getParameter("quantity");
+        int quantity = Integer.parseInt(request.getParameter("quantity"));
+
+        String selected = (String)session.getAttribute("selected");
+        Funds fund = (Funds)session.getAttribute(selected);
+
+        Stocks stock = fund.searchStock(search_stock);
+        stock.setQuantity(quantity);
 
         PrintWriter out = response.getWriter();
+
         try {
-            // get selected fund
-            String selected_fund = (String)session.getAttribute("selected");
-            Funds fund = (Funds)session.getAttribute(selected_fund);
-
-            Stocks stock = fund.searchStock(search_stock);
-            stock.setQuantity(Integer.parseInt(new_quantity));
-
             out.println("<HTML>\n" +
-                    "<HEAD><TITLE>Stock Found</TITLE></HEAD>\n" +
+                    "<HEAD><TITLE>Quantity Set</TITLE></HEAD>" +
                     "<BODY>\n" +
                     "<h1>Fund Management</h1>" +
                     "<h2>Quantity of Stock \"" + stock.getName() + "\" changed</h2>" +
+                    "Stock Name: " + stock.getName() +
+                    "<br> New Quantity: " + stock.getQuantity() + "<br/><br/>" +
                     "<a href=\"form_create_fund.jsp\">1. Create a new equity fund</a><br/>\n" +
                     "<a href=\"form_choose_fund.jsp\">2. Change the current equity fund</a><br/>\n" +
                     "<a href=\"form_search_stock.jsp\">3. Search for a stock with a particular name in the current equity fund</a><br/>\n" +
@@ -42,11 +44,12 @@ public class ChangeQuantity extends HttpServlet {
                     "<a href=\"form_change_quantity.jsp\">6. Change the quantity of a stock of the current equity fund</a><br/>" +
                     "</BODY></HTML>");
         } catch(Exception e) {
+            System.out.println(e);
             out.println("<HTML>\n" +
-                    "<HEAD><TITLE>Stock Found</TITLE></HEAD>\n" +
+                    "<HEAD><TITLE>Quantity Set</TITLE></HEAD>" +
                     "<BODY>\n" +
                     "<h1>Fund Management</h1>" +
-                    "<h2>Stock \"" + search_stock + "\" not found</h2>" +
+                    "<h2>Quantity of Stock \"" + stock.getName() + "\" couldn't be changed</h2>" +
                     "<a href=\"form_create_fund.jsp\">1. Create a new equity fund</a><br/>\n" +
                     "<a href=\"form_choose_fund.jsp\">2. Change the current equity fund</a><br/>\n" +
                     "<a href=\"form_search_stock.jsp\">3. Search for a stock with a particular name in the current equity fund</a><br/>\n" +
